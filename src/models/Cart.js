d2e893f7e5dc17a0611a1a8b8404f94dcc228bbb
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-01-10 05:52:53 -0300

    Se corrige modelo de carrito

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -2,31 +2,43 @@ const { Schema, default: mongoose } = require("mongoose");
 
 
 const CartSchema = new Schema ({
-    name: {
-        type: String
+    owner: {
+        type: mongoose.Schema.types.ObjetId,
+        ref: 'User'
     },
-    price:{
-        type: Number
-    },
-    quantity: {
-        type: Number
-    },
-    preferences: {
-        type: String
-    },
-    removed: [
+    products: [
         {
-            type: mongoose.Schema.types.ObjetId,
-            ref: 'Ingredient'
-        }
-    ],
-    toppings: [
-        {
-            type: mongoose.Schema.types.ObjetId,
-            ref: 'Ingredient'
+            name: {
+                type: String
+            },
+            price:{
+                type: Number
+            },
+            quantity: {
+                type: Number
+            },
+            preferences: {
+                type: String
+            },
+            removed: [
+                {
+                    type: mongoose.Schema.types.ObjetId,
+                    ref: 'Ingredient'
+                }
+            ],
+            toppings: [
+                {
+                    type: mongoose.Schema.types.ObjetId,
+                    ref: 'Ingredient'
+                }
+            ],
+            _id: false
         }
     ]
-})
+},{
+    versionKey: false
+}
+)
 
     const Cart = model('Cart', CartSchema);
 
