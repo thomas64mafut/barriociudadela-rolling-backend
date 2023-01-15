687b3a8b42e0fa0d300ca737ee3f29ca85e7cdfb
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-01-15 05:48:15 -0300

    correcciones en cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -4,17 +4,17 @@ const Cart = require("../models/Cart");
 const createCart = async(req,res) => {
     try {
         const {name, quantity, price, removed, toppings, size} = req.body
-        const cartFound = await Cart.findOne({owner: '63be41214108f8128df66295' })
+        const cartFound = await Cart.findOne({owner: '63bf51f9763505509f9f8921' })
         if(!cartFound){
             const payload = {
                 products: [
                     req.body,
                 ],
-                owner: '63be41214108f8128df66295'
+                owner: '63bf51f9763505509f9f8921'
             }
             const cart = new Cart(payload);
             await cart.save();
-            return res.status(200).json({message: 'Cart created correctly', payload});
+            return res.status(200).json({message: 'Product was added to the cart successfully', payload});
         }
         const productFoundCart = cartFound.products?.find((cartItem)=> (cartItem.name === name) && (removed.toString() === cartItem.removed.toString()) && (toppings.toString() === cartItem.toppings.toString()) && (size === cartItem.size))
         if(productFoundCart) {
@@ -32,10 +32,10 @@ const createCart = async(req,res) => {
                 return cartItem
             });
             const cartUpdate = await Cart.findByIdAndUpdate(cartFound._id, {products : newProductsList} , {new:true});
-            return res.status(200).json({message: 'Cart updated correctly', cartUpdate});
+            return res.status(200).json({message: 'Product was added to the cart successfully', cartUpdate});
         }
         const updatedCart = await Cart.findByIdAndUpdate(cartFound._id, {$push: {products: req.body}}, {new: true})
-        return res.status(200).json({message: 'Cart updated correctly', updatedCart});
+        return res.status(200).json({message: 'Product was added to the cart successfully', updatedCart});
     } catch (error) {
         res.status(error.code || 500).json({message : error.message})
     }
@@ -43,8 +43,8 @@ const createCart = async(req,res) => {
 
 const getCart = async (req, res) => {
     try {
-        const cart = await Cart.find()
-        res.status(200).json({message: 'Cart obtained correctly', cart})
+        const ownCart = await Cart.findOne({owner: '63bf51f9763505509f9f8921' }).populate('products.removed').populate('products.toppings')
+        res.status(200).json({message: 'Cart obtained correctly', ownCart})
     } catch (error) {
         res.status(error.code || 500).json({message : error.message})
         
