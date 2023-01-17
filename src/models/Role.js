42e9d180ec1c0c8c2b07c0d6a0178d3d0fb89082
thomas mafut <mafut50@gmail.com>
2023-01-17 15:18:14 -0300

    added isDeleted to role

--- src/models/Role.js
+++ src/models/Role.js
@@ -3,6 +3,10 @@ const { model, Schema } = require('mongoose');
 const RoleSchema = new Schema ({
     name: {
         type: String,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
     }
 }, {
     versionKey: false
