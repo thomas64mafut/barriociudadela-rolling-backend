9b07326beed687abe8ece3de34649a4cb7388629
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2023-01-23 17:22:35 -0300

    added token to cart logic (#9)

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createCart, getCart } = require('../controllers/cart');
+const { decodeToken } = require('../middlewares/auth');
 const { createCartValidations } = require('../middlewares/cart');
 const router = express.Router();
+decodeToken
 
-
-router.post('/', createCartValidations , createCart)
-router.get('/', getCart)
+router.post('/', decodeToken, createCartValidations , createCart)
+router.get('/', decodeToken, getCart)
 
 
 module.exports= router
