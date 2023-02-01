9180371cf26435b84967546716e757ba1753e40f
thomas mafut <mafut50@gmail.com>
2023-02-01 14:33:51 -0300

    added url modification

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json({ extended: true, limit: '50mb' }));
 app.use(express.urlencoded());
 app.use(cors({
-  origin: 'https://shimmering-cobbler-8dccda.netlify.app/',
+  origin: 'https://finalproyect-g2.netlify.app/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }));
 
