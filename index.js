9ee8ca7c964d7c53698d90a27b41dc25a53d8f9f
thomas mafut <mafut50@gmail.com>
2023-02-27 23:34:14 -0300

    added middleware for cors

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(cors());
 
 app.use('/api', require('./src/routes'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 mongoose.connect(process.env.DB_URL).then(() => {
   console.log('conectado a mongodb');
   server = app.listen(process.env.API_PORT, () => {
