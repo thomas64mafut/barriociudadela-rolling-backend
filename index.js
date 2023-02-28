e81bff03b693009e4d8053f6f9ab77b40f5eb421
thomas mafut <mafut50@gmail.com>
2023-02-28 12:22:20 -0300

    added more changes for vercel

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require('mongoose')
 const app = express();
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 
-app.use(express.json({ extended: true, limit: '50mb' }));
+app.use(express.json({ extended: true, limit: "50mb" }));
 app.use(express.urlencoded());
-app.use(cors());
 
-app.use('/api', require('./src/routes'));
+app.use(cors({ origin: process.env.ORIGIN_URL }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Max-Age", "86400");
+    next();
+});
+
+app.use("/api", require("./src/routes"));
 
 mongoose.connect(process.env.DB_URL).then(() => {
-  console.log('conectado a mongodb');
-  server = app.listen(process.env.API_PORT, () => {
-    console.log(`aplicacion escuchando en puerto ${process.env.API_PORT}`);
-  })
-})
+    console.log("conectado a mongodb");
+    server = app.listen(process.env.API_PORT, () => {
+        console.log(`aplicacion escuchando en puerto ${process.env.API_PORT}`);
+    });
+});
